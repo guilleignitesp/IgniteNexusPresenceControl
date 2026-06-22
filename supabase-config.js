55f9ef70ca15954384a49c2f3448f1f9802dbf6b
guilleignitesp <guillermo.morales.isp@gmail.com>
2026-06-22 19:19:50 +0200

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+// supabase-config.js
+// Configuración de conexión para el proyecto Supabase de IGNITE NEXUS
+const SUPABASE_URL = 'https://tdotkyqtvbvenwfmatgi.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_JGc0WAPRWnbqSo6y3Bt8zg_taKGD4iT';
